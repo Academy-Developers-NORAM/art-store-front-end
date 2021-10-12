3968bf86114d6917782391e624483a3f7cadd984
Ioana <ioanamedvesan@yahoo.com>
2021-10-12 12:27:12 -0500

    MC/IM Updating the logo image

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,13 @@ import { Navbar, Container, Image, Nav } from 'react-bootstrap';
 import { BsBagFill } from "react-icons/bs";
 import { BsFillPersonFill } from "react-icons/bs";
 import { BsSearch } from "react-icons/bs";
-import artStoreLogo from "../../src/art-store-logo.png";
 
 function Header() {
   return (
     <header>
         <Navbar bg="light" expand="lg">
             <Container>
-                <Image src={artStoreLogo} alt={"logo"} style={{width:"50px",height:"50px",padding:"10px"}}/>
+                <Image src='/art-store-logo.png' alt={"logo"} style={{width:"70px",height:"70px",padding:"10px"}}/>
                 <br/>
                 <Navbar.Brand href="#home">Academy Art Store</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,4 @@
 import { Carousel, } from 'react-bootstrap';
-import firstSlide from '../../src/images/SK-A-91.jpg';
-import secondSlide from '../../src/images/SK-A-358.jpg';
-import thirdSlide from '../../src/images/SK-A-1595.jpg';
-
-
 
 function Home() {
   return (
@@ -12,21 +7,21 @@ function Home() {
         <Carousel.Item>
             <img
             className="d-block w-50 h-50"
-            src={firstSlide}
+            src='images/SK-A-91.jpg'
             alt="First slide"
             />
         </Carousel.Item>
         <Carousel.Item>
             <img
             className="d-block w-50 h-50"
-            src={secondSlide}
+            src='/images/SK-A-358.jpg'
             alt="Second slide"
             />
         </Carousel.Item>
         <Carousel.Item>
             <img
             className="d-block w-50  h-50"
-            src={thirdSlide}
+            src='/images/SK-A-1595.jpg'
             alt="Third slide"
             />
         </Carousel.Item>
